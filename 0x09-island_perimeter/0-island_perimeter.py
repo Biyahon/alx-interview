f5c29b25063105432826274a9c306967add8c37d
root <biyahon@gmail.com>
2023-12-03 07:22:00 -0800

    perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
-def island_perimeter(grid):
-    perimeter = 0
+"""
+0-main
+"""
+island_perimeter = __import__('0-island_perimeter').island_perimeter
 
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1:
-                perimeter += 4
-
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-
-    return perimeter
-
-# Example usage
 if __name__ == "__main__":
     grid = [
         [0, 0, 0, 0, 0, 0],
